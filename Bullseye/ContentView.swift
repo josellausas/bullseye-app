bb7c32ef7b104337fe3100c9d18256eec9d33f77
Jose Llausas <jose@josellausas.com>
2020-12-12 17:26:27 -0600

    Initial commit

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+/// Main View
 struct ContentView: View {
+    
+    @State var showAlert: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Bullseye App")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+                .padding()
+            Button(action: {
+                print("Pressed!")
+                self.showAlert = true
+            }) {
+                Text("Hit Me")
+            }
+            .alert(isPresented: $showAlert) {
+                () -> Alert in
+                return Alert(title: Text("Hello there"), message: Text("First pop up!"), dismissButton: .default(Text("Awsome")))
+            }
+        }
     }
 }
 
