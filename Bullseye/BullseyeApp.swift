18508ef8fddfe920d69464f3d3b0d2db66b95897
Jose Llausas <jose@josellausas.com>
2020-12-12 16:59:36 -0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jose Llausas on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
