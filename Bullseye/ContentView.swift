30aad840160f56abd4e9df0d5c6a50c1aa30afb2
Jose <contact@josellausas.com>
2020-12-19 10:45:48 -0600

    Refactored views into functions

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -35,56 +35,83 @@ struct ContentView: View {
         return Spacer()
     }
     
-    var body: some View {
-        VStack {
-            Text("🎯 Bullseye App")
-                .font(.title)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
-                .padding()
-            HStack {
-                Text("Put the bullseye as close as you can to:")
-                    .fontWeight(.ultraLight)
-                Text("\(self.targetValue)")
-                    .fontWeight(.heavy)
-            }.padding()
-            
-            HStack {
-                Text("1")
-                Slider(value: $sliderValue)
-                Text("100")
-            }
-            
-            Button(action: self.buttonAction) {
-                Text("Hit Me!")
-            }
-                .padding()
-                .alert(isPresented: $showAlert) {
-                    () -> Alert in
-                        return Alert(
-                            title: Text("Hello there"),
-                            message: Text("The slider value is \(self.sliderValue)"),
-                            dismissButton: .default(Text("Awsome"))
-                        )
-                }
-            HStack {
-                Button(action:{}) {
-                    Text("Start Over")
-                }
-                s()
-                Text("Score: ")
-                Text("999999")
-                s()
-                Text("Round: ")
-                Text("999")
-                s()
-                Button(action:{}){
-                    Text("Info")
-                }
-            }.padding(.bottom, 20)
-            
+    func titleTextView(_ text: String) -> some View {
+        return Text(text)
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundColor(Color.green)
+            .padding()
+    }
+    
+    func instructionTextView(_ text: String) -> some View {
+        return Text(text)
+            .bold()
+            .kerning(2.0)
+            .multilineTextAlignment(.center)
+            .lineSpacing(4.0)
+            .font(.subheadline)
+    }
+    
+    func targetValueTextView() -> some View {
+        return Text("\(self.targetValue)")
+            .fontWeight(.heavy)
+    }
+    
+    func sliderWidget() -> some View {
+        return HStack {
+            Text("1")
+            Slider(value: $sliderValue)
+            Text("100")
         }
     }
+    
+    func hitMeButton() -> some View {
+        return Button(action: self.buttonAction) {
+            Text("Hit Me!")
+        }
+            .padding()
+            .alert(isPresented: $showAlert) {
+                () -> Alert in
+                    return Alert(
+                        title: Text("Hello there"),
+                        message: Text("The slider value is \(self.sliderValue)"),
+                        dismissButton: .default(Text("Awsome"))
+                    )
+            }
+    }
+    
+    func gameMenu() -> some View {
+        return HStack {
+            Button(action:{}) {
+                Text("Start Over")
+            }
+            s()
+            Text("Score: ")
+            Text("999999")
+            s()
+            Text("Round: ")
+            Text("999")
+            s()
+            Button(action:{}){
+                Text("Info")
+            }
+        }.padding(.bottom, 20)
+    }
+    
+    var body: some View {
+        VStack {
+            titleTextView("🎯 Bullseye App")
+            HStack {
+                instructionTextView("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO:")
+                targetValueTextView()
+            }.padding()
+            sliderWidget()
+            hitMeButton()
+            gameMenu()
+        }
+    }
+    
+    
     func getPointsForCurrentRound() -> Int {
         return abs(Int(self.targetValue) - Int(self.sliderValue))
     }
